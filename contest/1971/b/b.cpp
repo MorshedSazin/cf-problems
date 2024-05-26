4f0b3c96bfa93ecf02f918b141f526499fdc5cf8
MorshedSazin <95122430+MorshedSazin@users.noreply.github.com>
2024-05-26 11:32:53 +0600

    Update b.cpp

--- contest/1971/b/b.cpp
+++ contest/1971/b/b.cpp
@@ -1,40 +1,4 @@
-/*
-            _...._
-        _.dMMMMMMMMMb.
-    ..dMMMMMMMMMMMMMMMb
-  .dMMMMMMMMMMMMMMMMMMMMb.
- dMMMMMMMMMMMMMMMMMMMMMMMM.
- MMMMMMMP'`YMMMMMMMMMMMMMMMb
- MMMMMMP    MMMMMMMMMMMMMMMM
-dMMMMMP     `MMMMMMMMMMMMMMMb
-MMMMMM~=,,_  `MMMMMMMMMMMMMMM
-MMMMMMP,6;    `MMMMMMMMMMMMMMb
-MMMMMM|         ```^YMMMMMMMMM
-MMMMMM:   -~        / |MMMMMMMb
-`MMMMM/\  _.._     /  |MMMMMMMM
-  `YMM\_`.`~--'    \__/MMMMMMMM!
-    MMMMMM\       _.' _sS}MMMMMb
-    `YMMMMMb.__.sP.---.  MMMMMMM
-      ``YMMMMMMMP'        \MMMMMb
-          ``MMMd;          MMMMMM
-              dP|          :MMMMMb.
-          _.sP'             :MMMMMM
-      _.s888P'   ,  .-. .-. |MMMMM}
-   .s888888P    ,_|(  fsc  )|MMMM
- .d88888888;     `\ `-._.-' ;;M'
- 8888888888|       :         :;,
- 8888888888;       |         |`;,_
- `Y88888888b     _,:         |/Y\;
-    `^Y88888ssssSP~":        ;SsP/
-        """\        |         ;
-            ;       |         |
-            ;       ;         |
-           /      .'          |
-         .'    .-'            ;
-        /_...-'             .'\
-       .'              _..-'   :
-      /         __.--""         :
-------------------------------------------------*/
+
 /*
             _...._
         _.dMMMMMMMMMb.
@@ -77,20 +41,21 @@ using namespace std;
 
 void differentString() {
     
-    string s; cin >> s;
-    int j = s.size();
-    bool check = false;
-
-    for(int i = 0; i < s.size()/2; i++) {
-      if(s[i] != s[j-(i+1)]) {
-         check = true;
-         swap(s[i], s[j-(i+1)]);
-       }
+    string s;
+        cin >> s;
+        int f=0;
+        for (int i=0; i<s.size()-1; i++) {
+            if (s[i] != s[i+1]) {
+                f = 1;
+                char temp = s[i];
+                s[i] = s[i+1];
+                s[i+1] = temp;
+            }
+        }
+        if (f) {
+            cout << "Yes" << endl << s << endl;
+        } else cout << "No" << endl;
     }
-      if(check == true){
-        cout << "YES" << endl << s << endl;
-      } else cout << "NO" << endl;
-}
 
 int main() {
 int t; cin >> t;
